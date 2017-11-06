b82ae502e4ccc1e88ddcc726272df466659a9110
Jon Cowie <jonlives@gmail.com>
2017-11-06 14:00:01 +0000

    Remove test case that doesn't work on the CLI, add two more that do
    The evr case being tested here is not supported on the yum command line, and does not work in the existing yum provider. The two additional cases being added *do* work on the command line, and should be tested for here.
    
    Signed-off-by: Jon Cowie <jonlives@gmail.com>

--- spec/functional/resource/yum_package_spec.rb
+++ spec/functional/resource/yum_package_spec.rb
@@ -125,12 +125,20 @@ gpgcheck=0
         expect(shell_out("rpm -q chef_rpm").stdout.chomp).to match("^chef_rpm-1.2-1.fc24.x86_64$")
       end
 
-      it "works with an evr" do
+      it "works with an evra" do
         flush_cache
-        yum_package.package_name("chef_rpm-0:1.2-1.fc24")
+        yum_package.package_name("chef_rpm-0:1.2-1.fc24.x86_64")
         yum_package.run_action(:install)
         expect(yum_package.updated_by_last_action?).to be true
-        expect(shell_out("rpm -q chef_rpm").stdout.chomp).to match("^chef_rpm-1.2-1.fc24.x86_64$")
+        expect(shell_out("rpm -q chef_rpm").stdout.chomp).to eql("chef_rpm-1.2-1.fc24.x86_64")
+      end
+
+      it "works with version and release" do
+        flush_cache
+        yum_package.package_name("chef_rpm-1.2-1.fc24")
+        yum_package.run_action(:install)
+        expect(yum_package.updated_by_last_action?).to be true
+        expect(shell_out("rpm -q chef_rpm").stdout.chomp).to eql("chef_rpm-1.2-1.fc24.x86_64")
       end
 
       it "works with a version glob" do
